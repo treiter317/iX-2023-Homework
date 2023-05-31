fca418f78067423ac54a7229d85782b7ffc2305d
Miguel Villasenor <97707721+miguel1453@users.noreply.github.com>
2023-05-31 16:50:55 +0100

    Added my fib func

--- Day 3/fib.js
+++ Day 3/fib.js
@@ -1,3 +1,4 @@
+//Thomas Reiter's function
 function fibonacci (num) {
     let prev = 0;
     let current = 1;
@@ -15,3 +16,17 @@ function fibonacci (num) {
 }
 
 fibonacci(10);
+
+
+//Miguel's function
+const fibbonaci10 = () => {
+    let nums = [0, 1];
+
+    for (let i = 2; i < 10; i++) {
+        let num = nums[i-1] + nums[i-2];
+        nums.push(num);
+    }
+    return nums;
+};
+
+console.log(fibbonaci10());
